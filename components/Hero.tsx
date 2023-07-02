35b0933ac980fb8e08d48463e76608a13473b488
Harsh Saple <harshsaple.hs@gmail.com>
2023-07-02 12:40:27 +0530

    Responsive Handling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 function Hero() {
 
     const [text, count] = useTypewriter({
-        words: ["Hello! My Name is Harsh Saple!","ILoveToTravel.java","<codeMoreDebugLess />"],
+        words: ["Hello! My Name's Harsh Saple!","ILoveToTravel.java","<designInnovateImplement />"],
         loop: true,
         deleteSpeed: 60,
         delaySpeed: 2500,
@@ -20,19 +20,20 @@ function Hero() {
         <BackgroundShape/>
 
         <motion.div 
-        initial={{y: -500, opacity: 0, scale: 0.5}}
-        animate={{y: 0,opacity: 1,scale: 1}}
-        transition={{duration: 1.5}}
-        className='flex flex-row items-center'>
-            {/*Socials*/}
-            <img src={'/Harsh_Saple.jpg'} alt="Harsh's image" className='relative rounded-full h-40 w-40 mx-auto object-cover m-4'/>
+            initial={{y: -500, opacity: 0, scale: 0.5}}
+            animate={{y: 0,opacity: 1,scale: 1}}
+            transition={{duration: 1.5}}
+            className='flex flex-row items-center'>
+                {/*Socials*/}
+                <img src={'/Harsh_Saple.jpg'} alt="Harsh's image" className='relative rounded-full h-40 w-40 mx-auto object-cover m-4'/>
         </motion.div>
 
+        <h2 className='uppercase text-sm font-extralight tracking-[15px] pb-4'>Software Engineer</h2>
+
         <div className='z-20'>
-            <h2 className='uppercase text-sm font-extralight tracking-[15px] pb-4'>Software Engineer</h2>
             
-            <h1 className='text-3xl lg:text-5xl font-semi-bold text-white px-10'>
-                <span className='mr-3'>
+            <h1 className='text-2xl lg:text-4xl font-semi-bold text-white px-2'>
+                <span className='mr-2'>
                     {text}
                     <Cursor cursorColor='blue' cursorBlinking />
                 </span>
